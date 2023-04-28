ecd246eb883ec8f0393c12390171d35bb2488005
KASHISH <kashu80525@gmail.com>
2023-04-28 21:55:45 +0530

    adding arr method

--- block-BJaadh/code/index.js
+++ block-BJaadh/code/index.js
@@ -11,35 +11,52 @@ Clone the array before using sort method: [...arr]
 */
 
 // - Find the index of `101` in numbers
-
+numbers.indexOf("101");
 // - Find the last index of `9` in numbers
-
+numbers.lastIndexOf("9");
 // - Convert value of strings array into a sentance like "This is a collection of words"
-
+console.log(strings.join(" "));
 // - Add two new words in the strings array "called" and "sentance"
-
+strings.push("called" , "sentance")
 // - Again convert the updated array (strings) into sentance like "This is a collection of words called sentance"
-
+console.log(strings.join(" "));
 // - Remove the first word in the array (strings)
-
+strings.shift();
 // - Find all the words that contain 'is' use string method 'includes'
-
+console.log(strings.includes("is"));
 // - Find all the words that contain 'is' use string method 'indexOf'
 
 // - Check if all the numbers in numbers array are divisible by three use array method (every)
-
+numbers.every(function(number){
+ return number / 3;
+});
 // -  Sort Array from smallest to largest
+let original = [...numbers];
 
+function compareFunction(a , b){
+  //negative < 0 a;
+  //positive > 0 b;
+  //0
+  return a-b;
+}
+
+console.log(numbers.sort(compareFunction))
 // - Remove the last word in strings
-
+strings.pop();
 // - Find largest number in numbers
 
 // - Find longest string in strings
 
 // - Find all the even numbers
-
+console.log(numbers.filter(function isEven(number)
+{
+  return number % 2 === 0;
+}));
 // - Find all the odd numbers
-
+console.log(numbers.filter(function isOdd(number)
+{
+  return number % 2 !== 0;
+}))
 // - Place a new word at the start of the array use (unshift)
 
 // - Make a subset of numbers array [18,9,7,11]
@@ -64,7 +81,7 @@ var customers = [
 // - Create new array with only first name
 
 // - Create new array with all the full names (ex: "Joe Blogs")
-
+let fullNames = customers.join(firstname , secondname)
 // - Sort the array created above alphabetically
 
 // - Create a new array that contains only user who has at least one vowel in the firstname.
