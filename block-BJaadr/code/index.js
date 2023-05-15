abb0102486b0dc1154a89419da69181a3327cd6c
KASHISH <kashu80525@gmail.com>
2023-05-15 08:05:27 +0530

    adding codes using reduce

--- block-BJaadr/code/index.js
+++ block-BJaadr/code/index.js
@@ -1,11 +1,13 @@
 // NOTE: You can only use the (reduce) array method to solve this exercise:
 
 function countAllPeople() {
-  // your code goes here
+  got.houses.reduce((acc,cv) => {                                                                           
+    return acc + cv.people.length;
+  },0);
 }
 
 function peopleByHouses() {
-  // your code goes here
+  
 }
 
 function everyone() {
